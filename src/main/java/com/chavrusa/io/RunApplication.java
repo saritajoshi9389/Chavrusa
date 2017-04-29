283fbb9b1419287c3e004d453dcc5e5bc351b690
saritajoshi9389 <joshi.sar@husky.neu.edu>
2017-04-29 19:00:37 -0400

    Working template

--- src/main/java/com/chavrusa/io/RunApplication.java
+++ src/main/java/com/chavrusa/io/RunApplication.java
@@ -8,46 +8,45 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.chavrusa.repository.UserRepository;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses=com.chavrusa.repository.UserRepository.class)
 public class RunApplication {
 	private static final Logger log = LoggerFactory.getLogger(RunApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunApplication.class, args);
 	}
-	
-	@Bean
-	public CommandLineRunner demo(UserRepository repository) {
-		return (args) -> {
-			// save a couple of customers
-			repository.save(new User("Jack", "Bauer"));
-			repository.save(new User("Chloe", "O'Brian"));
-			repository.save(new User("Kim", "Bauer"));
-			repository.save(new User("David", "Palmer"));
-			repository.save(new User("Michelle", "Dessler"));
-
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (User customer : repository.findAll()) {
-				log.info(customer.toString());
-			}
-			log.info("");
-
-			// fetch an individual customer by ID
-			User customer = repository.findOne(1L);
-			log.info("Customer found with findOne(1L):");
-			log.info("--------------------------------");
-			log.info(customer.toString());
-			log.info("");
-
-			// fetch customers by last name
-			log.info("Customer found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			log.info("");
-		};
-	}
 }
+	
+//	@Bean
+//	public CommandLineRunner demo(UserRepository repository) {
+//		return (args) -> {
+//			// save a couple of customers
+//			repository.save(new User("Jack", "Bauer"));
+//			repository.save(new User("Chloe", "O'Brian"));
+//			repository.save(new User("Kim", "Bauer"));
+//			repository.save(new User("David", "Palmer"));
+//			repository.save(new User("Michelle", "Dessler"));
+//
+//			// fetch all customers
+//			log.info("Customers found with findAll():");
+//			log.info("-------------------------------");
+//			for (User customer : repository.findAll()) {
+//				log.info(customer.toString());
+//			}
+//			log.info("");
+//
+//			// fetch an individual customer by ID
+//			User customer = repository.findOne(1L);
+//			log.info("Customer found with findOne(1L):");
+//			log.info("--------------------------------");
+//			log.info(customer.toString());
+//			log.info("");
+//
+//			// fetch customers by last name
+//			log.info("Customer found with findByLastName('Bauer'):");
+//			log.info("--------------------------------------------");
+//			log.info("");
+//		};
+//	}
+//}
--- src/main/java/com/chavrusa/repository/UserRepository.java
+++ src/main/java/com/chavrusa/repository/UserRepository.java
@@ -1,9 +1,7 @@
-package com.chavrusa.repository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.chavrusa.io.User;
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
+//package com.chavrusa.repository;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.chavrusa.io.User;
+//public interface UserRepository extends CrudRepository<User, Long> {
+//
+//}
